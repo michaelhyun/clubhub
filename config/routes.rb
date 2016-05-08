38d543ec9cc2c75ea45977d868d37aa45046d057
michaelhyun <michael.hyun3@gmail.com>
2016-05-07 17:37:24 -0700

    The Master

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  
-  get 'clubs/displayTRY' => 'clubs#displayTRY'
+
+
   get 'clubs/show' => 'clubs#show'
   resources :clubs
   resources :categories
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get 'categories/display' => 'categories#display'
   get 'posts/index'
-    
+
     get 'admin/about'
 
   get 'sessions/new'
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
 get 'posts' => 'posts#index'
 get 'orgpages/index' => 'orgpages#org'
 
-controller :sessions do 
+controller :sessions do
 get 'login' => :new
 post 'login' => :create
 delete 'logout' => :destroy
@@ -85,5 +85,5 @@ root 'admin#index' #this is the homepage!
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
- 
+
 end
