35d02cc0ba27e4c2d05b463e4ee727b44d486756
Jo-Anna Marie Reyes <reyes.joannamarie@gmail.com>
2016-05-09 15:36:04 -0700

    adding rb and database stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508062518) do
+ActiveRecord::Schema.define(version: 20160508122026) do
 
   create_table "categories", force: true do |t|
     t.string   "genre"
