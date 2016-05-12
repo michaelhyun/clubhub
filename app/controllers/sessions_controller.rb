8b59c21b2de670c09eac9195e09517a1c92d4d54
Jo-Anna Marie Reyes <reyes.joannamarie@gmail.com>
2016-05-11 22:49:18 -0700

    changing small details

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,16 @@
 class SessionsController < ActionController::Base
 layout "users"
 skip_before_action :authorize
-
-
   def new
   end
-
-
-
 	def create
-    user = User.find_by(email: params[:session][:email].downcase)
-   if user && user.authenticate(params[:session][:password])
-    session[:user_id] = user.id
-    redirect_to user
-
+    	user = User.find_by(email: params[:session][:email].downcase)
+   		if user && user.authenticate(params[:session][:password])
+    	session[:user_id] = user.id
+    	redirect_to user
 		else
       render 'new'
 	  end
-
   end
 
   def destroy
