e388efd414949916d20c64b2da0d02711fe460fa
Jo-Anna Marie Reyes <reyes.joannamarie@gmail.com>
2016-05-12 10:40:47 -0700

    working on reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
-	attr_accessor :password
-=======
 	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: { maximum: 50 }
 			VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -14,6 +11,5 @@ class User < ActiveRecord::Base
 
   validates :password, presence: true, length: { minimum: 6 }
 
->>>>>>> origin/ka-branch
 	ratyrate_rater
 end
