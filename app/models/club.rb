a7320c4211d648925d5954e04cc26a1d10726fa3
Jo-Anna Marie Reyes <reyes.joannamarie@gmail.com>
2016-05-11 21:55:11 -0700

    committing changes to the user profile page

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,9 +1,5 @@
 class Club < ActiveRecord::Base
 	belongs_to :category
 	has_many :reviews
-<<<<<<< HEAD
-
-=======
 	ratyrate_rateable "rating"
->>>>>>> origin/ka-branch
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160511030729) do
-=======
 ActiveRecord::Schema.define(version: 20160511000225) do
->>>>>>> origin/ka-branch
 
   create_table "average_caches", force: true do |t|
     t.integer  "rater_id"
